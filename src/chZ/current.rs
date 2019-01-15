d385be0fa85c557445ed494a2bb8c30ac19f90e2
burrrata <42473928+burrrata@users.noreply.github.com>
2019-01-15 15:16:39 -0500

    Update current.rs

--- src/chZ/current.rs
+++ src/chZ/current.rs
@@ -1,7 +1,5 @@
 // GOAL
-//
-// Refactor the code so that you can change any part
-// and the "blockchain" still runs
+// - Refactor the code so that you can change any of the modules and it still runs.
 //
 // For example: 
 // - the code uses a function called hash()
@@ -9,28 +7,49 @@
 //   without chaning anything else and it all still works 
 //   because hash() just takes in data and returns a string
 
-// STF
+// Generic Blockchain:
+//  - State Transition Function
+//  - Data Encoding Function
+//  - Hash Function
+//  - Key Generation Function
+//  - Account Data
+//  - Transaction Data
+//  - State Data: a user defined configuration of the various blockchain modules
+
+// State Transition Function
 //  - determines what is a valid state transition by verifying tx
 //  - determines who is authorized to create a state change via PoA, PoW, PoS, etc...
 //  - impliments the state change
+//  - this needs to contain all params out of the box including the difficulty level
+//    and/or any functions needed to upgrade/modify those params
 
-// Data Encoding
+// Data Encoding Function
 //  - takes in arbitrary data and encodes it in a specific way
 //  - the entire "blockchain" uses this in order to allow any function
 //    to process arbitrary data inputs as well as sharing data between functions
+//  - standard for now, but may become upgradable as Ethreum and Substrate data is explored
 
-// Hash
+// Hash Function
 //  - takes in arbitrary data and returns a string
 //  - the way that data is hashes or the encoding of the string can be changed
 
-// Keys
+// Key Generation Function
 //  - the method to generate public and private key pairs
 //  - can be a centralized system, RSA, elliptic curves, etc...
+//  - contains all parmas neccessary to work out of the box
 
-// Accounts
+// Account Data
 //  - these will ALWAYS be a key/value pair in a HashMap
 //  - what you can change is the data that the account struct holds
 //  - UTXOs TBD
 
+// TX Data
+//  - standard for now
 
-
+// State Data
+//  - history: Vec<TX>
+//  - accounts: HashMap<i32, Account>
+//  - data encoding: user defined
+//  - State transition function: user defined
+//  - hash function: user defined
+//  - key gen function: user defined
